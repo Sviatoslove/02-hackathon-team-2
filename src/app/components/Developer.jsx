c7100b38d6abd9d392403bdebd2accae7e646909
goodAssistant <eestistone@gmail.com>
2023-06-25 09:29:01 +0300

    add feature/main-page

---
+++ src/app/components/Developer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Developer = ({ developer }) => {
+  return <div>Developrer</div>
+}
+
+Developer.propTypes = {
+  developer: PropTypes.object
+}
+
+export default Developer
